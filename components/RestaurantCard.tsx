e8dd0f3e7586a8fb714332d78e7e66816842b6c2
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-03-17 22:26:53 +0100

    wip

--- components/RestaurantCard.tsx
+++ components/RestaurantCard.tsx
@@ -4,7 +4,7 @@ import { FaBowlFood } from "react-icons/fa6";
 const RestaurantCard = () => {
     return (
         <div className="container mx-auto px-4 py-8">  {/* Added container for layout */}
-            <h1 className='md:text-2xl text-md my-4 text-right text-green-600 font-bold tracking-widest'>Location: Zaria</h1>
+            <h1 className='text-2xl my-4 text-right text-green-600 font-bold tracking-widest'>Location: Zaria</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-4 justify-end'>  {/* Use grid for responsiveness */}
                 {restaurants.map(restaurant => (
                     <div key={restaurant.id} className='bg-white shadow-lg p-4 px-12 rounded-lg'>
