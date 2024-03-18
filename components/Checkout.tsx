d0c2ee0f9fcfb66942e2f4d75b9bf220c3e4e9e4
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-03-18 13:19:45 +0100

    set isloading

--- components/Checkout.tsx
+++ components/Checkout.tsx
@@ -2,23 +2,26 @@ import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import { FiLoader } from 'react-icons/fi'
 
-const Checkout = ({cartTotal, fee, items, isLoading}:{cartTotal: number, fee: number, items: any, isLoading: boolean}) => {
+const Checkout = ({cartTotal, fee, items}:{cartTotal: number, fee: number, items: any}) => {
 
     const [email, setEmail] = useState('');
     const [foodId, setFoodId] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(false); 
+    const [isLoading, setIsLoading] = useState(false);
 
     const router = useRouter()
 
+    const totalPrice = cartTotal + fee;
     const handleCheckout = async (food: any) => {
         try {
+
+            setIsLoading(true);
 
             food.map((item: any) => {
                 setFoodId(item.food.id)
                 
             })
 
-            const totalPrice = cartTotal + fee;
 
             if (foodId) {
                 
@@ -48,19 +51,27 @@ const Checkout = ({cartTotal, fee, items, isLoading}:{cartTotal: number, fee: nu
         } catch (error) {
           console.error('Error navigating to food URL:', error);
           // Handle the error as needed
+        } finally {
+            setIsLoading(false); 
         }
     };
 
     return (
         <button
-        disabled={cartTotal < 400}
+        disabled={isNaN(totalPrice) || totalPrice < 400}
         onClick={() => handleCheckout(items)}
-        className={`w-fit px-4 py-2 ${cartTotal < 400 ? 'bg-gray-300 cursor-not-allowed' : 'border border-orange-600 hover:text-white hover:bg-orange-600'} mx-auto flex justify-center mt-4 rounded-md text-xl text-gray-700`}
+        className={`w-fit px-4 py-2 ${isNaN(totalPrice) || totalPrice < 400 ? 'bg-gray-300 cursor-not-allowed' : 'border border-orange-600 hover:text-white hover:bg-orange-600'} mx-auto flex justify-center mt-4 rounded-md text-xl text-gray-700`}
         >
             {isLoading ? (
-                <FiLoader className='w-4 h-4 animate-spin mr-1.5' />
-            ) : null}
-            Checkout
+                    <div className="fixed inset-0 z-50 flex items-center justify-center">
+                    <div className="fixed inset-0 bg-black opacity-50"></div>
+                    <FiLoader className='w-4 h-4 animate-spin mr-1.5' />
+                  </div>
+                ) : (
+                    'Checkout'
+                )
+            }
+            
         </button>
     )
 }
