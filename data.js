8feaca62e29fd15fdcddf9283a78105d7d2fd117
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-03-18 13:18:19 +0100

    set status to active

--- data.js
+++ data.js
@@ -3,41 +3,41 @@ export const restaurants = [
         id: 1,
         name: "Every Day Restaurant", 
         dishes: [
-            {id: "A1", name: "Jellof Rice", price: 800, imageLink: ''},
-            {id: "A2", name: "Fried Rice", price: 800, imageLink: ''},
-            {id: "A3", name: "Fried Yam", price: 800, imageLink: ''}
+            {id: "A1", name: "Jellof Rice", status: "Active", price: 800, imageLink: ''},
+            {id: "A2", name: "Fried Rice", status: "Active", price: 800, imageLink: ''},
+            {id: "A3", name: "Fried Yam", status: "Active", price: 800, imageLink: ''}
         ],
         addOn: [
-            {id: "A4", name: "Onion Sauce", price: 0, amount: 1, imageLink: ''},
-            {id: "A5", name: "Chicken", price: 1500, amount: 1, imageLink: ''},
-            {id: "A6", name: "Plantain", price: 200, amount: 1, imageLink: ''},
-            {id: "A7", name: "Boiled Eggs", price: 200, amount: 1, imageLink: ''},
-            {id: "A8", name: "Fried Eggs", price: 200, amount: 1, imageLink: ''},
-            {id: "A9", name: "Liver Sauce", price: 200, amount: 1, imageLink: ''},
-            {id: "A10", name: "Beaf", price: 200, amount: 1, imageLink: ''}
+            {id: "A4", name: "Onion Sauce", status: "Active", price: 0, amount: 1, imageLink: ''},
+            {id: "A5", name: "Chicken", status: "Active", price: 1500, amount: 1, imageLink: ''},
+            {id: "A6", name: "Plantain", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "A7", name: "Boiled Eggs", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "A8", name: "Fried Eggs", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "A9", name: "Liver Sauce", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "A10", name: "Beaf", status: "Active", price: 200, amount: 1, imageLink: ''}
         ],
         drinks: [
-            {id: "A11", name: "Bottle water", price: 150, amount: 1, imageLink: ''},
-            {id: "A12", name: "Coke 75cl", price: 250, amount: 1, imageLink: ''},
-            {id: "A13", name: "Fanta 75cl", price: 250, amount: 1, imageLink: ''},
-            {id: "A14", name: "Sprite 75cl", price: 250, amount: 1, imageLink: ''},
-            {id: "A15", name: "Five alive 100cl", price: 300, amount: 1, imageLink: ''},
-            {id: "A16", name: "Maltina can", price: 300, amount: 1, imageLink: ''},
+            {id: "A11", name: "Bottle water", status: "Active", price: 150, amount: 1, imageLink: ''},
+            {id: "A12", name: "Coke 75cl", status: "Active", price: 250, amount: 1, imageLink: ''},
+            {id: "A13", name: "Fanta 75cl", status: "Active", price: 250, amount: 1, imageLink: ''},
+            {id: "A14", name: "Sprite 75cl", status: "Active", price: 250, amount: 1, imageLink: ''},
+            {id: "A15", name: "Five alive 100cl", status: "Active", price: 300, amount: 1, imageLink: ''},
+            {id: "A16", name: "Maltina can", status: "Active", price: 300, amount: 1, imageLink: ''},
         ]
     },
     {
         id: 2,
         name: "Dine Restaurant",
         dishes: [
-            {id: "B1", name: "Matched Beans", otherName: 'Ewa Agoyin', price: 800, imageLink: ''},
-            {id: "B2", name: "Pounded Yam", price: 800, imageLink: ''}
+            {id: "B1", name: "Matched Beans", otherName: 'Ewa Agoyin', status: "Active", price: 800, imageLink: ''},
+            {id: "B2", name: "Pounded Yam", status: "Active", price: 800, imageLink: ''}
         ],
         addOn: [
-            {id: "B3", name: "Chicken", price: 1500, amount: 1, imageLink: ''},
-            {id: "B4", name: "Plantain", price: 200, amount: 1, imageLink: ''},
-            {id: "B5", name: "Boiled Eggs", price: 200, amount: 1, imageLink: ''},
-            {id: "B6", name: "Fried Eggs", price: 200, amount: 1, imageLink: ''},
-            {id: "B7", name: "Beaf", price: 200, amount: 1, imageLink: ''}
+            {id: "B3", name: "Chicken", status: "Active", price: 1500, amount: 1, imageLink: ''},
+            {id: "B4", name: "Plantain", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "B5", name: "Boiled Eggs", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "B6", name: "Fried Eggs", status: "Active", price: 200, amount: 1, imageLink: ''},
+            {id: "B7", name: "Beaf", status: "Active", price: 200, amount: 1, imageLink: ''}
         ]
     },
 ]
