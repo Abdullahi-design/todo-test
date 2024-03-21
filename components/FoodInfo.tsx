7d3c5b916559855340395462af2ff97dc1063eca
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-03-21 12:43:43 +0100

    match dishes and addOn to restaurants

--- components/FoodInfo.tsx
+++ components/FoodInfo.tsx
@@ -11,13 +11,13 @@ import { useCart } from "@/app/hooks/use-cart";
 
 const FoodInfo = ({ dish }: any) => {
 
+    const { addItemWithQuantity } = useCart()
     const [addOnQty, setAddOnQty] = useState<{ [key: string]: number }>({});
 
     const handleAddOnQuantityChange = (addOnId: string, quantity: number) => {
         setAddOnQty({ ...addOnQty, [addOnId]: quantity });
     };
 
-    const { addItemWithQuantity } = useCart()
     console.log(dish, 'man');
 
     const buyFood = (food: any) => {
@@ -37,8 +37,11 @@ const FoodInfo = ({ dish }: any) => {
                     // console.log(food, 'yam')
                 food && food.name && (
                     <div key={index} className='relative rounded-md my-20 bg-white shadow-lg p-6 py-12 px-32 justify-center'>
-                        <span className='top-0 left-0 absolute z-20 rounded-full'>
+                        <span className='flex top-0 left-0 absolute z-20 rounded-full'>
                             <GoDotFill className={`w-8 h-8 ${food.status == 'Active' ? 'text-green-700' : 'text-red-700'} `}/>
+                            <h1 className="text-gray-600 text-sm font-bold mt-2">
+                                <span>{restaurants.find((restaurant) => restaurant.dishes.find((dish: any) => dish.id === food.id))?.name}</span>
+                            </h1>
                         </span>
                         <span className=''><FaBowlRice className='w-32 md:w-48 h-32 md:h-48 text-center mx-auto text-orange-600' /></span>
                         <h1 className='text-gray-700 text-xl font-bold text-center my-2'>{food.name}</h1> 
@@ -54,48 +57,50 @@ const FoodInfo = ({ dish }: any) => {
             <h1 className='text-2xl mt-4 text-orange-600 font-bold tracking-widest md:absolute text-center right-[22rem] top-24'>Add On</h1>
             <div className="overflow-y-auto h-[26rem] md:w-1/2 w-full md:mt-20 mt-5 rounded-md">
                 {restaurants.map((restaurant) => (
-                    restaurant.addOn.map((addOn) => (
-                        <ul key={addOn.id} className='bg-white mx-auto shadow-lg w-fit gap-4 px-8 flex justify-between border-b py-2'>
-                            <div className="relative text-center">
-                                <span className='top-0 -left-6 absolute z-20 rounded-full'>
-                                    <GoDotFill className={`w-8 h-8 ${addOn.status == 'Active' ? 'text-green-700' : 'text-red-700'} `}/>
-                                </span>
-                                <span className=''><FaBowlRice className='w-12 h-12 text-center mx-auto text-orange-600' /></span>
-                                <h1 className='text-gray-600'>{addOn.name}</h1> 
-                                <h1 className='text-gray-600 text-sm font-bold text-center my-2'>Price: <span>₦{addOn.price}</span></h1>
-                                {/* <h1 className='text-gray-600 text-sm font-bold text-center my-2'>
-                                    Portion: 
-                                    <input type="number" name="" className="w-full flex rounded-lg mt-2 p-3 text-sm text-gray-500 outline-0" id={addOn.id} value={addOn.amount} />
-                                </h1> */}
-                                <label className="flex text-center justify-center">
-                                    <span className='font-semibold text-base text-gray-700'>
-                                        Portion {" "}
+                    dish.some((food: any) => restaurant.dishes.some((dish: any) => dish.id === food.id)) ? (
+                        restaurant.addOn.map((addOn) => (
+                            <ul key={addOn.id} className='bg-white mx-auto shadow-lg w-fit gap-4 px-8 flex justify-between border-b py-2'>
+                                <div className="relative text-center">
+                                    <span className='top-0 -left-6 absolute z-20 rounded-full'>
+                                        <GoDotFill className={`w-8 h-8 ${addOn.status == 'Active' ? 'text-green-700' : 'text-red-700'} `}/>
                                     </span>
-                                    <input
-                                        value={addOnQty[addOn.id] || ""}
-                                        onChange={(e) => handleAddOnQuantityChange(addOn.id, parseInt(e.target.value))}
-                                        type='number'
-                                        placeholder='1'
-                                        required
-                                        className='form_input'
+                                    <span className=''><FaBowlRice className='w-12 h-12 text-center mx-auto text-orange-600' /></span>
+                                    <h1 className='text-gray-600'>{addOn.name}</h1> 
+                                    <h1 className='text-gray-600 text-sm font-bold text-center my-2'>Price: <span>₦{addOn.price}</span></h1>
+                                    {/* <h1 className='text-gray-600 text-sm font-bold text-center my-2'>
+                                        Portion: 
+                                        <input type="number" name="" className="w-full flex rounded-lg mt-2 p-3 text-sm text-gray-500 outline-0" id={addOn.id} value={addOn.amount} />
+                                    </h1> */}
+                                    <label className="flex text-center justify-center">
+                                        <span className='font-semibold text-base text-gray-700'>
+                                            Portion {" "}
+                                        </span>
+                                        <input
+                                            value={addOnQty[addOn.id] || ""}
+                                            onChange={(e) => handleAddOnQuantityChange(addOn.id, parseInt(e.target.value))}
+                                            type='number'
+                                            placeholder='1'
+                                            required
+                                            className='form_input'
+                                        />
+                                        </label>
+                                    {/* <h1 className=''>Status: <span className='text-green-600'>Active</span> </h1> */}
+                                </div>
+                                <span>
+                                    <input 
+                                    type="checkbox" 
+                                    className="check_box" 
+                                    onClick={() => {
+                                        addItemWithQuantity(addOn, addOnQty[addOn.id] || 1);
+                                    }}
                                     />
-                                    </label>
-                                {/* <h1 className=''>Status: <span className='text-green-600'>Active</span> </h1> */}
-                            </div>
-                            <span>
-                                <input 
-                                type="checkbox" 
-                                className="check_box" 
-                                onClick={() => {
-                                    addItemWithQuantity(addOn, addOnQty[addOn.id] || 1);
-                                }}
-                                />
-                            </span>
-                            {/* <span onClick={() => addAddOn(addOn)} className='mt-12'>
-                                <IoMdAdd className=" cursor-pointer w-8 h-8 text-white mx-auto flex justify-end bg-orange-600 p-2 rounded-full mt-4 hover:bg-white hover:text-black border border-orange-600 transition delay-150"/>
-                            </span> */}
-                        </ul>
-                    ))
+                                </span>
+                                {/* <span onClick={() => addAddOn(addOn)} className='mt-12'>
+                                    <IoMdAdd className=" cursor-pointer w-8 h-8 text-white mx-auto flex justify-end bg-orange-600 p-2 rounded-full mt-4 hover:bg-white hover:text-black border border-orange-600 transition delay-150"/>
+                                </span> */}
+                            </ul>
+                        ))
+                    ): null
                 ))}
             </div>
         </>
