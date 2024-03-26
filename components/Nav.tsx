1d6d9c945f83dec1b939a63aa9b38566cfc29074
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-03-26 23:24:48 +0100

    logo

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useCart } from "@/app/hooks/use-cart";
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { FiShoppingCart } from "react-icons/fi";
@@ -16,13 +17,22 @@ const Nav = () => {
 
   return (
     <div className='border-b-[1.7px] border-black w-full flex justify-between'>
-      <Link href={'/'}>
+      {/* <Link href={'/'}>
         <h1 className="my-3 orange_gradient text-4xl font-extrabold leading-[1.15] text-black md:text-5xl">
           Ziype
         </h1>
+      </Link> */}
+      <Link href='/' className='gap-2 flex-center'>
+        <Image
+          src='/assets/images/ziype_logo.png'
+          alt='logo'
+          width={80}
+          height={80}
+          className='object-contain'
+        />
       </Link>
       <Link href={'/cart'}>
-        <FiShoppingCart  className="my-3 w-10 h-10 text-white p-2 rounded-lg bg-black cursor-pointer"/>
+        <FiShoppingCart  className="mt-5 w-10 h-10 text-white p-2 rounded-lg bg-black cursor-pointer"/>
         <span className="absolute ml-6 top-2 text-xs bg-orange-600 text-white py-1 px-2 rounded-full ">
           {isMounted ? itemCount : 0}
         </span>
