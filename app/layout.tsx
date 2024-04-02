cca96876d14acf2f962db905fe3c677e33f047f8
Abdullahi-design <abdullahismohd3000@gmail.com>
2024-04-02 19:33:57 +0100

    first init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Nav from "@/components/Nav";
-import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ziype",
-  description: "The fastest food delivery service in Nigeria",
+  title: "todo-test",
+  description: "Test app",
 };
 
 export default function RootLayout({
@@ -19,15 +17,10 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <div className='main'>
-          <div className='gradient' />
-        </div>
 
         <main className=''>
           <div className='app flex flex-col overflow-hidden min-h-screen'>
-            <Nav/>
             <div className='flex-grow flex-1'>{children}</div>
-            <Footer/>
           </div>
         </main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,8 @@
-import FoodCategory from "@/components/FoodCategory";
-import HeroPage from "@/components/HeroPage";
-import RestaurantCard from "@/components/RestaurantCard";
 
 export default function Home() {
   return (
     <main className="w-full text-center">
-      
-      {/* <HeroPage/> */}
-
-      <RestaurantCard/>
-
-      <FoodCategory/>
+      hello
     </main>
   );
 }
